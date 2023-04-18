b49e7dfc43feb4c83a1567037f8770ff3e75cad7
mnlencina <mnlencina@gmail.com>
2023-04-18 20:05:43 -0300

    vamos M3

---
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const utils = require("../utils/request");
+const process = require("process");
+
+//solo para pushear
+
+function pwd() {}
+
+function date() {}
+
+function echo() {}
+
+function ls() {}
+
+function cat() {}
+
+function head() {}
+
+function tail() {}
+
+function curl() {}
+
+module.exports = {};
